bebc43f9794cc5acab2bdcebb542de76637f66dd
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-09-28 02:31:12 +1000

    Improve linking.

--- TweetyLang.Emitter/Emitter.cs
+++ TweetyLang.Emitter/Emitter.cs
@@ -27,30 +27,18 @@ public static class Emitter
         var irBuilder = new IRBuilder();
         var modules = irBuilder.EmitProgram(tree.Root);
 
-        // Link all modules with LLVM linker
-        LLVMModuleRef module;
-        if (modules.Count == 1)
+        var module = modules[0];
+        for (int i = 1; i < modules.Count; i++)
         {
-            module = modules[0];
-        }
-        else
-        {
-            // Link all modules into the first module
-            module = modules[0];
-            for (int i = 1; i < modules.Count; i++)
+            var src = modules[i];
+            unsafe
             {
-                var src = modules[i];
-
-                unsafe
-                {
-                    int result = LLVM.LinkModules2(module, src);
-                    if (result != 0)
-                        throw new InvalidOperationException($"Failed to link module {i}.");
-                }
+                int result = LLVM.LinkModules2(module, src);
+                if (result != 0)
+                    throw new InvalidOperationException($"Failed to link module {i}.");
             }
         }
 
-        // Return singular module
         return module;
     }
 }
